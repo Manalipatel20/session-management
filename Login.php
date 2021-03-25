90cd973e80eba6fcc020260c13bffd2901d90ef1
Manalipatel20 <manalivadaliya@gmail.com>
2021-03-25 09:41:30 +0530

    ok

--- Login.php
+++ Login.php
@@ -18,37 +18,36 @@ class Login extends CI_Controller {
          
             redirect(base_url().'welcome');
         }
-        //if not load the login page
-        //$this->load->view('header');
+      
         $this->load->view('login_page');
-       // $this->load->view('footer');
+      
     }
 
     public function doLogin() {
-        //get the input fields from login form
+       
         $email = $this->input->post('email');
         $password = sha1($this->input->post('password'));
         
-        //send the email pass to query if the user is present or not
+    
         $check_login = $this->login->checkLogin($email, $password);
 
-        //if the result is query result is 1 then valid user
+       
         if ($check_login) {
-            //if yes then set the session 'loggin_in' as true
+           
             $this->session->set_userdata('logged_in', true);
             redirect(base_url().'welcome');
         } else {
-            //if no then set the session 'logged_in' as false
+           
             $this->session->set_userdata('logged_in', false);
             
-            //and redirect to login page with flashdata invalid msg
+        
             $this->session->set_flashdata('msg', 'Username / Password Invalid');
             redirect(base_url().'login');            
         }
     }
 
     public function logout() {
-        //unset the logged_in session and redirect to login page
+       
         $this->session->unset_userdata('logged_in');
         redirect(base_url().'login');
     }
